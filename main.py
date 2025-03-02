7c73bd473130335058d499b70681f4e33d7773fa
Matthew Tysoe <37555494+DavePasta@users.noreply.github.com>
2025-03-02 21:37:31 +0000

    Update main.py
    added a main guard for better structure

--- main.py
+++ main.py
@@ -2,15 +2,6 @@ import pandas as pd
 import os
 from itertools import combinations
 
-base_file_path = r""
-
-excel_files = [f for f in os.listdir(base_file_path) if f.endswith((".xlsx", "xls"))]
-
-file_data = {
-    file_name: pd.read_excel(os.path.join(base_file_path, file_name))
-    for file_name in excel_files
-}
-
 def compare_files(file_name1: str, df1: pd.DataFrame, file_name2: str, df2: pd.DataFrame) -> None:
 
     store_ids1 = set(df1["Store ID"].unique())
@@ -42,5 +33,17 @@ def compare_files(file_name1: str, df1: pd.DataFrame, file_name2: str, df2: pd.D
         print(f"{file_name1} and {file_name2}: Overall Sell Out mismatch ({total_sellout1:,} vs {total_sellout2:,})")
     print("-" * 60)
 
-for (name1, df1), (name2, df2) in combinations(file_data.items(), 2):
-    compare_files(name1, df1, name2, df2)
+
+if __name__ == "__main__":
+
+    base_file_path = r"C:\Users\mattt\Desktop\Sales Data Comparison Files"
+
+    excel_files = [f for f in os.listdir(base_file_path) if f.endswith((".xlsx", "xls"))]
+
+    file_data = {
+        file_name: pd.read_excel(os.path.join(base_file_path, file_name))
+        for file_name in excel_files
+    }
+
+    for (name1, df1), (name2, df2) in combinations(file_data.items(), 2):
+        compare_files(name1, df1, name2, df2)
